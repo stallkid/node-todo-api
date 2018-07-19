d9fb3fcf15fa82f4819b410651d89a181fc9b343
Renan L. Bianchini <renan@waiodev.com.br>
2018-07-19 09:39:24 -0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5b4f9b06aa7f224085c4a2c8')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: true
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b4cfce9897155348c970754')
+    }, {
+            $set: {
+                name: 'teste'
+            },
+            $inc: {
+                age: 1
+            }
+        }, {
+            returnOriginal: false
+        }).then((result) => {
+            console.log(result);
+        });
+
+
+
+    // db.close();
+});

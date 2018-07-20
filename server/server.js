e211de7deeb1dcea9ad2bd03a55283b070f8d2b2
Renan L. Bianchini <renan@waiodev.com.br>
2018-07-20 17:07:54 -0300

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -111,8 +111,11 @@ app.post('/users', (req, res) => {
     var body= _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    user.save().then((user) => {
-        res.send(user);
+    user.save().then(() => {
+        return user.generateAuthToken();
+        // res.send(user);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });

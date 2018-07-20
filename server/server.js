4854d58e1a7c2ad5502b98d953afbafd8d5635ef
Renan L. Bianchini <renan@waiodev.com.br>
2018-07-20 10:29:34 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-var { mongoose } = require('./db/mongoose')
-var { Todo } = require('./models/todos')
-var { User } = require('./models/user')
+const { mongoose } = require('./db/mongoose')
+const { Todo } = require('./models/todos')
+const { User } = require('./models/user')
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
-
+// POST TODOS
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -20,14 +21,36 @@ app.post('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
-
+// GET ALL TODOS
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
-})
+});
+// GET ONE TODO
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({
+            message: 'Id not Valid',
+            status: 404
+        });
+    }
+    Todo.findById(id).then((todos) => {
+        if (!todos) {
+            res.status(400).send({
+                message: 'Id not Found',
+                status: 400
+            });
+        }
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
